7df20467f7efb4a63cdf4a8f0fbb376ddddc099b
Jamie Pak <happyjpak@gmail.com>
2019-12-04 15:00:34 +0900

    실행 콘솔 출력되도록 수정

--- scripts/execute-deploy.sh
+++ scripts/execute-deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source $HOME/.bash_profile
-export JAMIE=$HOME/jamie_project/deploy/jamie_common
-
 cd /home/ec2-user/jamie_project/docker-image
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+./deploy.sh 
+#> /dev/null 2> /dev/null < /dev/null &
